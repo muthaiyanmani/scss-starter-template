a5ee08f4d4bb7597393ec957491064711d3b1e4c
Muthaiyan Mani <muthaiyan.mb@zohocorp.com>
2022-12-19 12:57:41 +0530

    sass webpack boilerplate init

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import '../scss/app.scss';
+
+/* Your JS Code goes here */
+
+// eslint-disable-next-line import/no-self-import
+import './app.js';
---
+++ src/js/home.js
@@ -0,0 +1,4 @@
+const home = () => 'Hello World!';
+
+// eslint-disable-next-line no-console
+console.log(home());
